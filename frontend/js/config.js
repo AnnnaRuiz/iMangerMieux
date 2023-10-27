653d2c36ab7d8b662d5748f2ad8686c06b94aaaa
Anna Ruiz <anna.ruiz@free.fr>
2023-10-27 17:10:28 +0200

    config js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,1 +1,8 @@
-let apiURL = 'http://localhost/iMangerMieux/backend'; 
+let apiURL; 
+
+if (window.location.pathname.includes('wamp64/www')) {
+    apiURL = 'http://localhost/iMangerMieux/backend';
+} else {
+    apiURL = 'http://localhost:8888/iMangerMieux/backend';
+}
+
