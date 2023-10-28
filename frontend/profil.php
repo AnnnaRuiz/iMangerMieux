5ab24e58d5f219f7be178af14720d07acdae816f
Anna Ruiz <anna.ruiz@free.fr>
2023-10-28 11:10:43 +0200

    front profil

--- frontend/profil.php
+++ frontend/profil.php
@@ -1,4 +1,4 @@
-<div class="container center mb-3">
+<div class="container mb-3">
     <div class="row">
         <div class="col-12">
             <div class="row mb-1">
@@ -8,12 +8,27 @@
             </div>
             <div class="row border border-dark bg-light rounded justify-content-center">
                 <div class="col-5 m-3 border rounded">
-                    <h4>Données de connexion</h4>
+                    <h4 class="text-center">Données de connexion</h4>
+                    <hr/>
+                    <p>Nom : <?php ?></p>
+                    <p>Email : <?php ?></p>
+                    <p class="border rounded d-inline-flex"> Mot de passe <?php ?></p>
+                    <div class="row m-3">
+                        <button type="button" class="btn btn-primary ml-auto" onclick="modifyUserAccount(this)">Modifier</button>
+                    </div>
                 </div>
                 <div class="col-5 m-3 border rounded">
-                    <h4>Informations sur l'utilisateur</h4>
+                    <h4 class="text-center">Informations sur l'utilisateur</h4>
+                    <hr/>
+                    <p>Sexe : <?php ?></p>
+                    <p>Poids : <?php ?></p>
+                    <p>Taille : <?php ?></p>
+                    <p>Âge : <?php ?></p>
+                    <div class="row m-3">
+                        <button type="button" class="btn btn-primary ml-auto" onclick="modifyUserInfo(this)">Modifier</button>
+                    </div>
                 </div>
-            </div>
+            </div> 
         </div>
     </div>
 </div>
