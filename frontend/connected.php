0cdd8d96d0b4600288785864abf6cd653c414139
Anna Ruiz <anna.ruiz@free.fr>
2023-10-30 11:44:29 +0100

    session ok, à continuer

--- frontend/connected.php
+++ frontend/connected.php
@@ -2,22 +2,23 @@
 
     // on simule une base de données
     $users = array(
-        // login => password
+        //email => pwd
         'riri@mail' => 'fifi',
-        'maitre@jedi' => 'luke');
+        'maitre@jedi' => 'luke'
+    );
 
     $errorText = "";
     $successfullyLogged = false;
 
-    if(isset($_POST['login']) && isset($_POST['password'])) {
-        $tryLogin=$_POST['login'];
-        $tryPwd=$_POST['password'];
+    if(isset($_POST['email']) && isset($_POST['pwd'])) {
+        $tryLogin=$_POST['email'];
+        $tryPwd=$_POST['pwd'];
     // si login existe et password correspond
         if( array_key_exists($tryLogin,$users) && $users[$tryLogin]==$tryPwd ) {
             $successfullyLogged = true;
             $login = $tryLogin;
             session_start();
-            $_SESSION['login']=$login;
+            $_SESSION['email']=$login;
 
         } else {
              $errorText = "Erreur de login/password";
@@ -28,10 +29,8 @@
         $errorText = "Merci d'utiliser le formulaire de login";
     }
     if($successfullyLogged) {
-        
-        // ecrire ici --> TO DO 
         echo "<h1>Bienvenue ".$login."</h1>";
-        echo "<a href='deconnexion.php'>Se déconnecter </a>";
+        echo "<a href='index.php?deconnexion'>Se déconnecter </a>";
         
     } else {
         echo $errorText;
--- frontend/index.php
+++ frontend/index.php
@@ -11,44 +11,74 @@
 
     $login = "Anonymous";
     $is_connected = False;
+    
 
-    if(isset($_SESSION['login'])) {
-        $login = $_SESSION['login'];
+    if(isset($_SESSION['email'])) {
+        $login = $_SESSION['email'];
         $is_connected = True;
     }
 
-    if(isset($_POST['login'])) {
-        $login = $_POST['login'];
-        $_SESSION['login'] = $login;
+    if(isset($_POST['email'])) {
+        $login = $_POST['email'];
+        $_SESSION['email'] = $login;
         $is_connected = True;
     }
 ?>
 <?php 
-    require_once('template_header.php'); 
+    require_once('template_header.php');
     require_once("template_menu.php");
 ?>
 
 <body>
-
 <?php
 
     $currentPageId = 'accueil';
     if(isset($_GET['page'])){
         $currentPageId=$_GET['page'];
     }
-    echo "<h3 class='text-center bg-light m-0'>Bienvenue ".$login."</h3>";
+
     renderMenuToHTML($currentPageId);
+    echo "<h5>Bienvenue ".$login."</h5>";
 
-   $pageToInclude = $currentPageId . ".php";
-   if(is_readable($pageToInclude)){
-      require_once($pageToInclude);
-   }   
-   else{
-      require_once("error.php");
-   }
+    $pageToInclude = $currentPageId . ".php";
 
+    if(is_readable($pageToInclude)){
+        require_once($pageToInclude);
+    }   
+    else{
+        require_once("error.php");
+    }
 
-require('template_footer.php') ?>
+?>
+
+<?php if (!$is_connected): ?>
+    
+    <div class="container justify-content-center d-flex align-items-center mb-3" >
+        <div class="card p-4">
+            <h2 class="text-center mb-4">Connexion</h2>
+            <form id="login_form" action="connected.php" method="post">
+                <div class="form-group">
+                    <label for="email">Adresse Email</label>
+                    <input type="email" class="form-control" id="email" name="email" placeholder="Votre email">
+                </div>
+                <div class="form-group">
+                    <label for="pwd">Mot de passe</label>
+                    <input type="password" class="form-control" id="pwd" name="pwd" placeholder="Votre mot de passe">
+                </div>
+                <button type="submit" class="btn btn-primary btn-block">Se Connecter</button>
+            </form>
+        </div>
+    </div>
+
+<?php else: ?>
+
+    <div>
+        <a href="index.php?deconnexion">Déconnexion</a>
+    </div>
+
+<?php endif ?>
+
+<?php require('template_footer.php') ?>
 
 </body>
 
