c1577aa8b4b7a3e1bc62622fc3f4388cade008c2
Charreyron Anthony <charreyronanthony0263@gmail.com>
2023-11-07 11:58:19 +0100

    Backend journal total

--- backend/journal.php
+++ backend/journal.php
@@ -1,9 +1,19 @@
 <?php
 session_start();
 require_once('functionsAPI.php');
+$date = date("Y-m-d");
 
-$mail= $_SESSION['mail'];
-$result = extractDailyDatas($mail);
-header('Content-type: application/json');
-http_response_code(200);
-exit(json_encode($result));
+switch($_SERVER["REQUEST_METHOD"]){
+    case 'POST':
+        $mail= $_SESSION['mail'];
+        $result1 = extractDailyDatas($mail, $date);
+        $result2 = extractWeeklyDatas ($mail, $date);
+        $result3 = extractMonthlyDatas($mail, $date);
+        header('Content-type: application/json');
+        http_response_code(200);
+        echo json_encode(["dailyData" => $result1, "weeklyData" => $result2, "monthlyData" => $result3]);
+        break;
+    default : 
+        http_response_code(501);
+        exit(json_encode(["message" => "Not implemented"]));
+}
