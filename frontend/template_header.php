6ea01e2f837645bd8c6a7292ae6cd381f8761d65
Charreyron Anthony <charreyronanthony0263@gmail.com>
2023-11-09 00:20:54 +0100

    Recherche aliment pour le repas

--- frontend/template_header.php
+++ frontend/template_header.php
@@ -3,6 +3,7 @@
     <meta name="viewport">
 
     <link href="css/style.css" rel="stylesheet">
+    <link href="css/search.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link href="//cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" rel="stylesheet">
